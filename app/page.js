904711b782ba5827ab8689394e20c100168dca54
Irene <manyeinhtuttin@gmail.com>
2026-07-21 00:13:10 +0630

    Update page.js

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default async function HomePage() {
   const folders = await getFolders();
 
   return (
-    <div className="min-h-screen flex flex-col bg-[#D8D0C7]">
+    <div className="min-h-screen flex flex-col bg-[#F0E6DA]">
       <PublicHeader />
 
       <main className="flex-1 max-w-5xl mx-auto w-full px-4 sm:px-6 py-8 sm:py-14">
