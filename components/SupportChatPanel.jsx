1c16ddb75e7ddd47407952502e0a87bf0d52e94b
Irene <manyeinhtuttin@gmail.com>
2026-08-03 19:01:39 +0630

    Fix formatting for attachment link in SupportChatPanel

--- components/SupportChatPanel.jsx
+++ components/SupportChatPanel.jsx
@@ -132,7 +132,7 @@ export default function SupportChatPanel({ phone, role, onClose }) {
                       </a>
                     )}
                     {m.attachment_url && m.attachment_type === "file" && (
-                      
+                    <a
                         href={m.attachment_url}
                         target="_blank"
                         rel="noopener noreferrer"
